411672de7c9d460d29e9190e67104ede0d01757f
杜立恒 <2020080096@secoder.net>
2022-03-30 22:36:58 +0800

    initialise unit test

--- passenger/tests.py
+++ passenger/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class PassengerModelTests(TestCase):
+    def setUp(self):
+        pass
+
+    # Tests for tokens
+    def test_obtain_passenger_token(self):
+        pass
+
+    def test_invalid_passenger_code(self):
+        payload = {
+            'code': 'invalidusercode'
+        }
+        response = self.client.post('/api/passenger/token',
+                                    data=payload,
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_token_verify(self):
+        payload = {
+            'token': 'invalidtoken11223344'
+        }
+        response = self.client.post('/api/passenger/token',
+                                    data=payload,
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 400)
