271b1b5d123fde8561ac02ee67bf6d48bd02b37f
杜立恒 <2020080096@secoder.net>
2022-04-06 10:18:02 +0800

    added unit test for passenger info API

--- passenger/tests.py
+++ passenger/tests.py
@@ -98,3 +98,40 @@ class PassengerTokenTest(TestCase):
                                     data=payload,
                                     content_type='application/json')
         self.assertEqual(response.status_code, 400)
+
+
+class PassengerInfoTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_info(self):
+        access_token, refresh_token, status_code = authenticate(self)
+        self.assertEqual(status_code, 200)
+        response = self.client.get(
+            '/api/passenger/info',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_info(self):
+        access_token, refresh_token, status_code = authenticate(self)
+        self.assertEqual(status_code, 200)
+        payload = {
+            'username': 'updatedUsername',
+        }
+        response = self.client.post(
+            '/api/passenger/info',
+            data=payload,
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(payload, response.data)
+
+        # Get the user info again
+        response = self.client.get(
+            '/api/passenger/info',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(payload, response.data)
