325521bfb2a58def60caaf27070fcd30a49e24c5
杜立恒 <2020080096@secoder.net>
2022-04-10 23:28:09 +0800

    test creating new order

--- passenger/testcases/order.py
+++ passenger/testcases/order.py
@@ -1,7 +1,8 @@
 from .utils import *
+from order.models import Order
 
 
-class PassengerOrderTest(TestCase):
+class PassengerEstimatePriceTest(TestCase):
     def setUp(self):
         pass
 
@@ -58,6 +59,68 @@ class PassengerOrderTest(TestCase):
         }
         response = self.client.post(
             '/api/passenger/order/est-price',
+            data=payload,
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 400)
+
+
+class PassengerCreateOrderTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_create_order(self):
+        access_token, refresh_token, status_code = authenticate(self)
+        self.assertEqual(status_code, 200)
+
+        payload = {
+            "start_POI_name": "清华大学",
+            "start_POI_address": "北京市海淀区双清路30号",
+            "start_POI_lat": "39.99970025463166",
+            "start_POI_long": "116.32636879642432",
+            "end_POI_name": "故宫博物院",
+            "end_POI_address": "中国北京市东城区景山前街4号",
+            "end_POI_lat": "39.9136172322172",
+            "end_POI_long": "116.39729231302886",
+            "passenger_lat": "39.99970025463166",
+            "passenger_long": "116.32636879642432"
+        }
+        response = self.client.post(
+            '/api/passenger/order/new',
+            data=payload,
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_current_order_exists(self):
+        access_token, refresh_token, status_code = authenticate(self)
+        self.assertEqual(status_code, 200)
+
+        payload = {
+            "start_POI_name": "清华大学",
+            "start_POI_address": "北京市海淀区双清路30号",
+            "start_POI_lat": "39.99970025463166",
+            "start_POI_long": "116.32636879642432",
+            "end_POI_name": "故宫博物院",
+            "end_POI_address": "中国北京市东城区景山前街4号",
+            "end_POI_lat": "39.9136172322172",
+            "end_POI_long": "116.39729231302886",
+            "passenger_lat": "39.99970025463166",
+            "passenger_long": "116.32636879642432"
+        }
+        response = self.client.post(
+            '/api/passenger/order/new',
+            data=payload,
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+
+        # Adds another order when an order exists
+        response = self.client.post(
+            '/api/passenger/order/new',
             data=payload,
             content_type='application/json',
             **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
