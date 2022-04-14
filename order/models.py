8b96128949040acea7fed355bc26f4db057465ab
杜立恒 <2020080096@secoder.net>
2022-04-14 20:54:30 +0800

    style test for order app

--- order/models.py
+++ order/models.py
@@ -4,8 +4,12 @@ from driver.models import Driver
 
 
 class Order(models.Model):
-    passenger = models.ForeignKey(Passenger, on_delete=models.DO_NOTHING)
-    driver = models.ForeignKey(Driver, blank=True, null=True, on_delete=models.DO_NOTHING)
+    passenger = models.ForeignKey(Passenger,
+                                  on_delete=models.DO_NOTHING)
+    driver = models.ForeignKey(Driver,
+                               blank=True,
+                               null=True,
+                               on_delete=models.DO_NOTHING)
 
     start_POI_name = models.CharField(max_length=150)
     start_POI_address = models.CharField(max_length=150)
@@ -16,19 +20,23 @@ class Order(models.Model):
     end_POI_address = models.CharField(max_length=150)
     end_POI_lat = models.FloatField()
     end_POI_long = models.FloatField()
-    
+
     # Encode a list of coordinates with Base64
     before_pickup_path = models.TextField()
     after_pickup_path = models.TextField()
     # Distance based on after_pickup_path
-    distance = models.DecimalField(default='0', max_digits=6, decimal_places=2)
+    distance = models.DecimalField(default='0',
+                                   max_digits=6,
+                                   decimal_places=2)
 
     est_price = models.DecimalField(max_digits=6, decimal_places=2)
-    real_price = models.DecimalField(default='0', max_digits=6, decimal_places=2)
+    real_price = models.DecimalField(default='0',
+                                     max_digits=6,
+                                     decimal_places=2)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField()
-    
+
     # After the order is fulfilled
     ended_at = models.DateTimeField(blank=True, null=True)
     status = models.PositiveIntegerField()
