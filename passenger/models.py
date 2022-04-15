60076bfc2be45aaba7b53d35c2cc799aca1138a2
杜立恒 <2020080096@secoder.net>
2022-04-15 11:26:52 +0800

    reset migrations

--- passenger/models.py
+++ passenger/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Passenger(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     username = models.CharField(max_length=150)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    identification_no = models.CharField(max_length=20)
     phone = models.CharField(max_length=11)
     latitude = models.FloatField(blank=True, null=True)
     longitude = models.FloatField(blank=True, null=True)
