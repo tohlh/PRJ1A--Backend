97f3d938575b43d70da74f3bcc6059acaa12bc2a
2020080099 <2020080099@secoder.net>
2022-04-15 22:10:21 +0900

    add invalid post info test for driver

--- driver/testcases/info.py
+++ driver/testcases/info.py
@@ -40,3 +40,24 @@ class DriverInfoTest(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(payload, response.data)
+
+    def test_invalid_post_info(self):
+        access_token, refresh_token, status_code = authenticate(self)
+        self.assertEqual(status_code, 200)
+        payload = {
+            'username': 'updatedUsername',
+            'carplate': '京123123',
+            'phone': '01234567890',
+            'age': 30,
+        }
+
+        response = self.client.post(
+            '/api/driver/info',
+            data=payload,
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual({
+            'ErrMsg': 'identification_no is required'
+            }, response.data)
