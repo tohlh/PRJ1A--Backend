41c509b072bd5d9e94e3276085106c9ebb043645
杜立恒 <2020080096@secoder.net>
2022-04-23 16:33:16 +0800

    separates driver and passenger auth appid

--- passenger/auth.py
+++ passenger/auth.py
@@ -1,13 +1,9 @@
+import requests
 from .models import Passenger
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt_wechat_sso.serializers import (
-    TokenObtainPairSerializer
-)
-from rest_framework_simplejwt_wechat_sso.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
+from django.utils.translation import gettext_lazy as _
+from rest_framework_simplejwt_wechat_sso.serializers import *
+from rest_framework_simplejwt_wechat_sso.views import *
 
 
 class PassengerTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -16,6 +12,113 @@ class PassengerTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = cls.token_class.for_user(user)
         token["type"] = "passenger"
         return token
+
+    def WeChatSSO(cls, code):
+        if code == "superuser0":
+            return {
+                "access_token": "ACCESS_TOKEN",
+                "expires_in": 7200,
+                "refresh_token": "REFRESH_TOKEN",
+                "openid": "superuser0openid123123123",
+                "scope": "SCOPE",
+                "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+            }
+        elif code == "superuser1":
+            return {
+                "access_token": "ACCESS_TOKEN",
+                "expires_in": 7200,
+                "refresh_token": "REFRESH_TOKEN",
+                "openid": "superuser1openid123123123",
+                "scope": "SCOPE",
+                "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+            }
+        elif code == "superuser2":
+            return {
+                "access_token": "ACCESS_TOKEN",
+                "expires_in": 7200,
+                "refresh_token": "REFRESH_TOKEN",
+                "openid": "superuser2openid123123123",
+                "scope": "SCOPE",
+                "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+            }
+        elif code == "superuser3":
+            return {
+                "access_token": "ACCESS_TOKEN",
+                "expires_in": 7200,
+                "refresh_token": "REFRESH_TOKEN",
+                "openid": "superuser3openid123123123",
+                "scope": "SCOPE",
+                "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+            }
+        elif code == "superuser4":
+            return {
+                "access_token": "ACCESS_TOKEN",
+                "expires_in": 7200,
+                "refresh_token": "REFRESH_TOKEN",
+                "openid": "superuser4openid123123123",
+                "scope": "SCOPE",
+                "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+            }
+        elif code == "superuser5":
+            return {
+                "access_token": "ACCESS_TOKEN",
+                "expires_in": 7200,
+                "refresh_token": "REFRESH_TOKEN",
+                "openid": "superuser5openid123123123",
+                "scope": "SCOPE",
+                "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+            }
+        elif code == "superuser6":
+            return {
+                "access_token": "ACCESS_TOKEN",
+                "expires_in": 7200,
+                "refresh_token": "REFRESH_TOKEN",
+                "openid": "superuser6openid123123123",
+                "scope": "SCOPE",
+                "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+            }
+        elif code == "superuser7":
+            return {
+                "access_token": "ACCESS_TOKEN",
+                "expires_in": 7200,
+                "refresh_token": "REFRESH_TOKEN",
+                "openid": "superuser7openid123123123",
+                "scope": "SCOPE",
+                "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+            }
+        elif code == "superuser8":
+            return {
+                "access_token": "ACCESS_TOKEN",
+                "expires_in": 7200,
+                "refresh_token": "REFRESH_TOKEN",
+                "openid": "superuser8openid123123123",
+                "scope": "SCOPE",
+                "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+            }
+        elif code == "superuser9":
+            return {
+                "access_token": "ACCESS_TOKEN",
+                "expires_in": 7200,
+                "refresh_token": "REFRESH_TOKEN",
+                "openid": "superuser9openid123123123",
+                "scope": "SCOPE",
+                "unionid": "o6_bmasdasdsad6_2sgVt7hMZOPfL"
+            }
+        req_params = {
+            'appid': api_settings.WECHAT_APP_ID,
+            'secret': api_settings.WECHAT_APP_SECRET,
+            'js_code': code,
+            'grant_type': 'authorization_code'
+        }
+        url = 'https://api.weixin.qq.com/sns/jscode2session'
+
+        response = requests.get(url, params=req_params)
+        result = response.json()
+
+        if 'errcode' in result:
+            msg = _(result['errmsg'])
+            raise serializers.ValidationError(msg, code='authorization')
+        return result
 
     def create_or_get_user(cls, openid):
         user, _ = get_user_model().objects.get_or_create(
