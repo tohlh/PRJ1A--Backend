71bfaeb42d67734e8f0b3b78c5ba071a7a125f68
杜立恒 <2020080096@secoder.net>
2022-04-30 18:50:31 +0800

    add message model

--- message/models.py
+++ message/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from driver.models import Driver
 
-# Create your models here.
+
+class DriverMessage(models.Model):
+    driver = models.ForeignKey(Driver,
+                               blank=True,
+                               null=True,
+                               on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    value = models.CharField(max_length=150)
+    color = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
