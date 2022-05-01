5aa00050ada3decfc8d6be9cb8437de5b44d3f1a
杜立恒 <2020080096@secoder.net>
2022-05-01 16:09:52 +0800

    test_end_order

--- driver/testcases/order.py
+++ driver/testcases/order.py
@@ -638,3 +638,16 @@ class DriverUnregisteredTests(TestCase):
         )
         self.assertEqual(response.status_code, 402)
         self.assertEqual(response.data['errMsg'], '请填写个人资料。')
+
+    def test_end_order(self):
+        access_token, refresh_token, status_code = auth_driver(self,
+                                                               'superuser1')
+        self.assertEqual(status_code, 200)
+
+        response = self.client.post(
+            '/api/driver/order/end',
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 402)
+        self.assertEqual(response.data['errMsg'], '请填写个人资料。')
