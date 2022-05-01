9b1ce429a9b3935425136e120e6cacd8d9fc6048
杜立恒 <2020080096@secoder.net>
2022-05-01 18:48:23 +0800

    test_current_order

--- passenger/testcases/order.py
+++ passenger/testcases/order.py
@@ -828,3 +828,15 @@ class PassengerInvalidAuthTests(TestCase):
             **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
         )
         self.assertEqual(response.status_code, 401)
+
+    def test_current_order(self):
+        access_token, refresh_token, status_code = auth_driver(self,
+                                                               'superuser1')
+        self.assertEqual(status_code, 200)
+
+        response = self.client.get(
+            '/api/passenger/order/current',
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 401)
