100e043456527c94bae1e1b6abfcb60f3b0fcf80
杜立恒 <2020080096@secoder.net>
2022-05-01 09:58:40 +0800

    add passenger test_reset_info

--- passenger/testcases/info.py
+++ passenger/testcases/info.py
@@ -59,3 +59,44 @@ class PassengerInfoTest(TestCase):
             **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
         )
         self.assertEqual(response.status_code, 400)
+
+    def test_reset_info(self):
+        access_token, refresh_token, status_code = auth_passenger(self,
+                                                                  'superuser0')
+        self.assertEqual(status_code, 200)
+
+        payload = {
+            'username': 'updatedUsername',
+            'phone': '01234567890',
+            'age': 30,
+            'identification_no': '012345678901233457'
+        }
+        response = self.client.post(
+            '/api/passenger/info',
+            data=payload,
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(payload, response.data)
+
+        response = self.client.post(
+            '/api/passenger/info/reset',
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual({}, response.data)
+
+        response = self.client.get(
+            '/api/passenger/info',
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual({
+            'username': '',
+            'phone': '',
+            'age': None,
+            'identification_no': ''
+        }, response.data)
