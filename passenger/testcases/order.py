547c94490c5a0725327a5024e5d1634dc4e77d6b
杜立恒 <2020080096@secoder.net>
2022-05-01 18:24:14 +0800

    test_update_location

--- passenger/testcases/order.py
+++ passenger/testcases/order.py
@@ -726,3 +726,21 @@ class PassengerUnregisteredTests(TestCase):
         )
         self.assertEqual(response.status_code, 402)
         self.assertEqual(response.data['errMsg'], '请填写个人资料。')
+
+    def test_update_location(self):
+        access_token, refresh_token, status_code = auth_passenger(self,
+                                                                  'superuser0')
+        self.assertEqual(status_code, 200)
+
+        payload = {
+            "latitude": 39.99970025463180,
+            "longitude": 116.32636879642432
+        }
+        response = self.client.post(
+            '/api/passenger/order/paid',
+            data=payload,
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 402)
+        self.assertEqual(response.data['errMsg'], '请填写个人资料。')
