190b44c048bc16dfde48dbbd11a8f758b09118b1
杜立恒 <2020080096@secoder.net>
2022-05-01 15:59:03 +0800

    test_list_messages

--- driver/testcases/message.py
+++ driver/testcases/message.py
@@ -28,3 +28,32 @@ class PassengerEstimatePriceTest(TestCase):
             **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
         )
         self.assertEqual(response.status_code, 200)
+
+    def test_list_messages(self):
+        access_token, refresh_token, status_code = auth_driver(self,
+                                                               'superuser0')
+        self.assertEqual(status_code, 200)
+
+        payload = {
+            'title': 'Test message title',
+            'description': 'This is a description of this test message',
+            'value': 'some value',
+            'color': 'purple'
+        }
+
+        for i in range(0, 100):
+            response = self.client.post(
+                '/api/driver/msg',
+                data=payload,
+                content_type='application/json',
+                **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+            )
+            self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(
+            '/api/driver/msg',
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 100)
