457304095fa2f1625e4ca1d1fb899b066a244b09
杜立恒 <2020080096@secoder.net>
2022-05-01 18:49:31 +0800

    test_update_location

--- passenger/testcases/order.py
+++ passenger/testcases/order.py
@@ -864,3 +864,20 @@ class PassengerInvalidAuthTests(TestCase):
             **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
         )
         self.assertEqual(response.status_code, 401)
+
+    def test_update_location(self):
+        access_token, refresh_token, status_code = auth_driver(self,
+                                                               'superuser1')
+        self.assertEqual(status_code, 200)
+
+        payload = {
+            "latitude": 39.99970025463180,
+            "longitude": 116.32636879642432
+        }
+        response = self.client.post(
+            '/api/passenger/order/paid',
+            data=payload,
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 401)
