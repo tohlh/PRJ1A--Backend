3a3adbcf7322db1ede38fdcfadfed947584fe9c4
杜立恒 <2020080096@secoder.net>
2022-05-01 18:22:40 +0800

    test_current_order

--- passenger/testcases/order.py
+++ passenger/testcases/order.py
@@ -687,3 +687,16 @@ class PassengerUnregisteredTests(TestCase):
         )
         self.assertEqual(response.status_code, 402)
         self.assertEqual(response.data['errMsg'], '请填写个人资料。')
+
+    def test_current_order(self):
+        access_token, refresh_token, status_code = auth_passenger(self,
+                                                                  'superuser0')
+        self.assertEqual(status_code, 200)
+
+        response = self.client.get(
+            '/api/passenger/order/current',
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': f'Bearer {access_token}'}
+        )
+        self.assertEqual(response.status_code, 402)
+        self.assertEqual(response.data['errMsg'], '请填写个人资料。')
